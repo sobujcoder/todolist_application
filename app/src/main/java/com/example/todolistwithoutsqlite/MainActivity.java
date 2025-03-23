4baee254b0acf00b4a1b2c646be89c0ea3d2a79f
sobujcoder <87200928+sobujcoder@users.noreply.github.com>
2025-03-23 19:10:13 +0600

    Code

---
+++ app/src/main/java/com/example/todolistwithoutsqlite/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.todolistwithoutsqlite;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+
+        setupListViewListener();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener((adapter, item, pos, id) -> {
+            items.remove(pos);
+            itemsAdapter.notifyDataSetChanged();
+            return true;
+        });
+    }
+
+    public void onAddItem(View view) {
+        EditText etNewItem = findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+}
